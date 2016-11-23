c75699182fab971c62cf3f5ce8307331936520f1
Pauk <paukku.palikka@zbitbytes.com>
2016-11-23 19:54:45 +0200

    added handlers for backspace and enter

--- ui.c
+++ ui.c
@@ -32,9 +32,18 @@ int ui_update()
 	break;
     case KEY_RIGHT:
 	curid++;
+	if (buffer_getch(b, curid) == '\0') curid--;
 	break;
     case KEY_LEFT:
 	curid--;
+	break;
+    case KEY_BACKSPACE:
+	buffer_delete(b, curid, 1);
+	curid--;
+	break;
+    case KEY_ENTER:
+	buffer_insertch(b, '\n', curid);
+	curid++;
 	break;
     default:
 	buffer_insertch(b, (char)ch, curid);
@@ -45,17 +54,37 @@ int ui_update()
     /* update the screen */
     int i = 0;
     int cy = 0, cx = 0;
+    int ccx = 0, ccy = 0;
     char c = buffer_getch(b, i);
     while (c != '\0') {
-	mvaddch(cy, cx, c);
-	cx++;
+	switch(c) {
+	case '\t':
+	    mvaddstr(cy, cx, "    ");
+	    cx += 4;
+	    break;
+	case '\n':
+	    clrtoeol();
+	    cx = 0;
+	    cy++;
+	default:
+	    mvaddch(cy, cx, c);
+	    cx++;
+	}
+
 	if (cx > cols) {
 	    cy++;
 	    cx = 0;
 	}
+
+	if (i == curid) {
+	    ccx = cx;
+	    ccy = cy;
+	}
+	
 	i++;
 	c = buffer_getch(b, i);
     }
+    move(ccy, ccx - 1);
 
     refresh();
     
